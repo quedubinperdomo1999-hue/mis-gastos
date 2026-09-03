dd0df13d85d2206e66921ae752c500360ae4cdaf
quedubinperdomo1999-hue <quedubinperdomo1999@gmail.com>
2026-09-03 09:45:21 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,38 @@
-const CACHE='querux-v18';
-const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
+const CACHE='querux-v2-20260903';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./mascota-ingreso.jpg','./mascota-gasto.jpg'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).catch(()=>{}));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const req=event.request;
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put('./index.html',copy));
+        return res;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(req).then(cached=>cached||fetch(req).then(res=>{
+      if(new URL(req.url).origin===location.origin){
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(req,copy));
+      }
+      return res;
+    }))
+  );
+});
